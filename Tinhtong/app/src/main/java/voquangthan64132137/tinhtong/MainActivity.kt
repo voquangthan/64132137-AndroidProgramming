e023103e8b7e7c91b2abad587d54d16fb34f1109
voquangthan <voquangthan1234@gmail.com>
2025-02-28 19:38:06 +0700

    1

--- Tinhtong/app/src/main/java/voquangthan64132137/tinhtong/MainActivity.kt
+++ Tinhtong/app/src/main/java/voquangthan64132137/tinhtong/MainActivity.kt
@@ -1,6 +1,9 @@
 package voquangthan64132137.tinhtong
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +12,15 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val Inputa= findViewById<EditText>(R.id.inputa)
+        val Inputb= findViewById<EditText>(R.id.inputb)
+        val Button1= findViewById<Button>(R.id.btn1)
+        val Result= findViewById<TextView>(R.id.result)
+        Button1.setOnClickListener{
+            TinhTong(Inputa,Inputb,Result)
+
+        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +28,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private  fun TinhTong(Inputa:EditText,Inputb:EditText,Result:TextView){
+        val a=Inputa.text.toString().toInt()
+        val b=Inputb.text.toString().toInt()
+        val sum=a+b;
+        Result.text="kết quả là : $sum"
     }
 }
