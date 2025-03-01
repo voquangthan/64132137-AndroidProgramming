532f1ad3b4232410c9e2a94646d4c22c3f515798
voquangthan <voquangthan1234@gmail.com>
2025-03-01 08:42:51 +0700

    done

--- TinhBMI/app/src/main/java/com/example/tinhbmi/MainActivity.kt
+++ TinhBMI/app/src/main/java/com/example/tinhbmi/MainActivity.kt
@@ -19,18 +19,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val Chieucao = findViewById<TextView>(R.id.input1)
-        val Cannang = findViewById<TextView>(R.id.input2)
+        val Chieucao = findViewById<EditText>(R.id.input1)
+        val Cannang = findViewById<EditText>(R.id.input2)
         val btn = findViewById<Button>(R.id.btn)
-        val Kq = findViewById<EditText>(R.id.kq)
+        val Kq = findViewById<TextView>(R.id.kq)
 
         btn.setOnClickListener{
-
+            TinhToan(
+                Chieucao, Cannang, Kq,
+            )
         }
     }
-    private fun TinhToan(Chieucao:TextView,Cannang:TextView,Kq:EditText){
+    private fun TinhToan(Chieucao:EditText,Cannang:EditText,Kq:TextView){
                 val a=Chieucao.text.toString().toDouble()
                 val b=Cannang.text.toString().toDouble()
-                if()
+                val BMI=b/(a*a)
+                val KetQua= when{
+                    BMI < 18.5 -> "cân nặng thấp gầy"
+                    BMI in 18.5..24.9 -> "bình thường"
+                    BMI in 25.0..29.9 ->"tiền béo phì"
+                    BMI in 30.0..34.9 ->"béo phì 1"
+                    BMI in 35.0..39.9 ->"béo phì 2"
+                    BMI >=40.0 ->"béo phì 3"
+                    else -> "ko xác định"
+                }
+            Kq.text="Kết quả là:$KetQua"
+
     }
 }
