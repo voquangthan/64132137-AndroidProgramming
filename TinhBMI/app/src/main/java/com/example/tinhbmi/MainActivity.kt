5698c2cfac7ff389daf81eea1c6418ab373ad33c
voquangthan <voquangthan1234@gmail.com>
2025-03-01 08:18:18 +0700

    1

--- TinhBMI/app/src/main/java/com/example/tinhbmi/MainActivity.kt
+++ TinhBMI/app/src/main/java/com/example/tinhbmi/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tinhbmi
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val Chieucao = findViewById<TextView>(R.id.input1)
+        val Cannang = findViewById<TextView>(R.id.input2)
+        val btn = findViewById<Button>(R.id.btn)
+        val Kq = findViewById<EditText>(R.id.kq)
+
+        btn.setOnClickListener{
+
+        }
+    }
+    private fun TinhToan(Chieucao:TextView,Cannang:TextView,Kq:EditText){
+                val a=Chieucao.text.toString().toDouble()
+                val b=Cannang.text.toString().toDouble()
+                if()
     }
 }
