46690ac96e9a94ecce0c20e3b75ee33470a805bc
voquangthan <voquangthan1234@gmail.com>
2025-03-01 21:21:19 +0700

    1

--- ex4_congtrunhanchia/app/src/main/java/com/example/ex4_congtrunhanchia/MainActivity.kt
+++ ex4_congtrunhanchia/app/src/main/java/com/example/ex4_congtrunhanchia/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ex4_congtrunhanchia
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val a= findViewById<EditText>(R.id.input1)
+        val b= findViewById<EditText>(R.id.input2)
+        val BtnCong= findViewById<Button>(R.id.btncong)
+        val BtnTru= findViewById<Button>(R.id.btntru)
+        val BtnNhan= findViewById<Button>(R.id.btnn)
+        val BtnChia= findViewById<Button>(R.id.btnchia)
+        val KQ= findViewById<TextView>(R.id.kq)
+        BtnCong.setOnClickListener{
+            TinhTong(a, b, KQ)
+        }
+
+
+    }
+    private fun TinhTong(a:EditText,b:EditText,KQ:TextView){
+        val soa=a.text.toString().toInt()
+        val sob=b.text.toString().toInt()
+        val sum=soa+sob
+        KQ.text="kết quả cộng là: $sum"
     }
 }
