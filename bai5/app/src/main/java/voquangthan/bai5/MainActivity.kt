8614c9535d2133ef10dcfc3a85d9b112204747c4
voquangthan <voquangthan1234@gmail.com>
2025-03-04 18:18:28 +0700

    3

--- bai5/app/src/main/java/voquangthan/bai5/MainActivity.kt
+++ bai5/app/src/main/java/voquangthan/bai5/MainActivity.kt
@@ -1,10 +1,14 @@
 package voquangthan.bai5
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val Soa= findViewById<EditText>(R.id.input1)
+        val Sob= findViewById<EditText>(R.id.input2)
+        val Cong= findViewById<Button>(R.id.btncong)
+        val Tru= findViewById<Button>(R.id.btntru)
+        val Chia= findViewById<Button>(R.id.btnchia)
+        val KQ= findViewById<TextView>(R.id.kq)
+       Tru.setOnClickListener(){
+            Tong(Soa,Sob,KQ)
+       }
+        Cong.setOnClickListener(){
+            Hieu(Soa,Sob,KQ)
+
+        }
+        Chia.setOnClickListener(){
+            Tich(Soa,Sob,KQ)
+
+        }
+
+
+
+    }
+    private fun Tong(Soa:EditText,Sob:EditText,KQ:TextView){
+        val A=Soa.text.toString().toInt()
+        val B=Sob.text.toString().toInt()
+        val rs=A+B
+        KQ.text="KQ là:$rs"
+    } private fun Hieu(Soa:EditText,Sob:EditText,KQ:TextView){
+        val A=Soa.text.toString().toInt()
+        val B=Sob.text.toString().toInt()
+        val rs=A-B
+        KQ.text="KQ là:$rs"
+    } private fun Tich(Soa:EditText,Sob:EditText,KQ:TextView){
+        val A=Soa.text.toString().toInt()
+        val B=Sob.text.toString().toInt()
+        val rs=A*B
+        KQ.text="KQ là:$rs"
     }
 }
