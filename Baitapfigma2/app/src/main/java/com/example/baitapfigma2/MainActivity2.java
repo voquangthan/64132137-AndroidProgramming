5c48b65afd5bc680cd7c0b07aeb9a58733a2af51
voquangthan <voquangthan1234@gmail.com>
2025-03-23 11:13:04 +0700

    3
    3

--- Baitapfigma2/app/src/main/java/com/example/baitapfigma2/MainActivity2.java
+++ Baitapfigma2/app/src/main/java/com/example/baitapfigma2/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.baitapfigma2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,23 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+        Button Btndem=findViewById(R.id.btndem);
+        Button Btnmatch=findViewById(R.id.btnmatch);
+        Btndem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+        Btnmatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+            }
         });
     }
 }
