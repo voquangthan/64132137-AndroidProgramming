07ba031aa98e03cf834c30f0cb86da079f5d2419
voquangthan <voquangthan1234@gmail.com>
2025-03-31 01:33:10 +0700

    3

---
+++ Baitapgiuaky/app/src/main/java/com/example/baitapgiuaky/Activitycau2.java
@@ -0,0 +1,45 @@
+package com.example.baitapgiuaky;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class Activitycau2 extends AppCompatActivity {
+    ListView Lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_activitycau2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Lv=findViewById(R.id.lv1);
+        ArrayList<String> mang= new ArrayList<>();
+        mang.add("xin chào 1");
+        mang.add("xin chào 2");
+        mang.add("xin chào 3");
+        mang.add("xin chào 4");
+        mang.add("xin chào 5");
+        ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,mang);
+        Lv.setAdapter(adapter);
+        Lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+}
---
+++ Baitapgiuaky/app/src/main/java/com/example/baitapgiuaky/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.baitapgiuaky;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button Btn1;
+    Button Btn2;
+    Button Btn3;
+    Button Btn4;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Btn1=findViewById(R.id.btn1);
+        Btn2=findViewById(R.id.btn2);
+        Btn3=findViewById(R.id.btn3);
+        Btn4=findViewById(R.id.btn4);
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,Activitycau1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
