740c3607dc1e4428fbbe6e558ebcccb973006986
voquangthan <voquangthan1234@gmail.com>
2025-04-16 06:26:11 +0700

    3

--- FragmentDynamic/app/src/main/java/com/example/fragmentdynamic/MainActivity.java
+++ FragmentDynamic/app/src/main/java/com/example/fragmentdynamic/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.holder, new BlankFragment())
+                .add(R.id.footer,new FooterFragment())
+                .commit();
     }
 }
