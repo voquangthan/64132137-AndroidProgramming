a028bd084b11c442e2cf7ad76f6bd1fd0b57d450
voquangthan <voquangthan1234@gmail.com>
2025-04-23 09:36:33 +0700

    3
    3

--- nav_drawer/app/src/main/java/com/example/nav_drawer/MainActivity.java
+++ nav_drawer/app/src/main/java/com/example/nav_drawer/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.nav_drawer;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,25 +22,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        bottomNavigationView=findViewById(R.id.botnav1);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int duocchon= item.getItemId();
-                if(duocchon==R.id.menu_home){
-                    Toast.makeText(MainActivity.this,"đây là home",Toast.LENGTH_SHORT).show();
-                }
-                else  if(duocchon==R.id.menu_search){
-                    Toast.makeText(MainActivity.this,"đây là search",Toast.LENGTH_SHORT).show();
-                }
-                else  if(duocchon==R.id.menu_profile){
-                    Toast.makeText(MainActivity.this,"đây là profile",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    return false;
-                }
-                return true;
-            }
-        });
+
     }
 }
